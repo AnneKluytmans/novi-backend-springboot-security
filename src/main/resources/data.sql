5fbe54b50ec39a36378be1ff9890c454779db550
AnneKluytmans <annekluytmans1@gmail.com>
2025-01-10 11:32:05 +0100

    Implemented basic security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,8 @@ VALUES
     (2011, 'toyota', 'yaris'),
     (2017, 'alfa romeo', '156')
 ;
+
+INSERT INTO users (username, password, role) VALUES ('admin', '$2a$10$TAqU/aDX166qnqnZN3pGDeaNsjUwQa.g0pvLUgF/Glr7RO.SIW2Ly', 'ADMIN');
+INSERT INTO users (username, password, role) VALUES ('user', '$2a$10$YAyMWw.R.NRlw6hLgLhSWu.eJ7kqsMicjJTTPTrxMiqLT0IMYKquO', 'USER');
+
+
