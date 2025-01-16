621ac9235a2a4ebb60b601a3e79241b51e0dd183
AnneKluytmans <annekluytmans1@gmail.com>
2025-01-16 15:10:24 +0100

    Basic authentication en authorization

--- src/main/java/nl/novi/cardemo/CardemoApplication.java
+++ src/main/java/nl/novi/cardemo/CardemoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CardemoApplication.class, args);
     }
