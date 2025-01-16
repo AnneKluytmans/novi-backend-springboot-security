0c37c31856d1c904c85d5f9f973528393cf66dba
AnneKluytmans <annekluytmans1@gmail.com>
2025-01-16 16:34:34 +0100

    Implemented JWT authenticatie

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,7 @@ VALUES
     (2017, 'alfa romeo', '156')
 ;
 
-INSERT INTO users (username, password, role) VALUES ('admin', '$2a$10$TAqU/aDX166qnqnZN3pGDeaNsjUwQa.g0pvLUgF/Glr7RO.SIW2Ly', 'ADMIN');
+INSERT INTO users (username, password, role) VALUES ('admin', '$2a$10$dHGCjegyx30PicT5uJSTb.I3zhn7vmawIbM6mbpPz2kEA4OB.GtMG', 'ADMIN');
 INSERT INTO users (username, password, role) VALUES ('user', '$2a$10$YAyMWw.R.NRlw6hLgLhSWu.eJ7kqsMicjJTTPTrxMiqLT0IMYKquO', 'USER');
 
 
